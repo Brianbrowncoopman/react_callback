33f7dc6f2399bdc3da9213889cc04c9869cd0b93
Brian Brown Coopman <brianbrowncoopman@gmail.com>
2022-08-25 21:41:45 -0400

    1 tercerproyecto

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import './index.css';
+import React from 'react';
+import { useEffect, useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          25-8
+          seguimos
+        </p>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
